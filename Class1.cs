9c1cb223878d064f173e0a68ce5e3cff5ce9bd2e
Renzo <renzo.remuzgo@tecsup.edu.pe>
2024-09-10 21:18:56 -0500

    first commit

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eva01
+{
+    public class Proveedor
+    {
+        public string idProveedor { get; set; }
+        public string nombreCompañia { get; set; }
+        public string nombrecontacto { get; set; }
+        public string cargocontacto { get; set; }
+        public string direccion { get; set; }
+
+    }
+
+    public class Categoria
+    {
+        public string idcategoria { get; set; }
+        public string nombrecategoria { get; set; }
+        public string descripcion { get; set; }
+        public string Activo { get; set; }
+        public string CodCategoria { get; set; }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+
+namespace Eva01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string cadena = "Server=LAB1507-13\\SQLEXPRESS03; Database=NeptunoB; Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(cadena))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("USP_ListarProveedores", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                List<Proveedor> listaProveedores = new List<Proveedor>();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Proveedor proveedor = new Proveedor
+                        {
+                            idProveedor = reader["idproveedor"].ToString(),
+                            nombreCompañia = reader["nombrecompañia"].ToString(),
+                            nombrecontacto = reader["nombrecontacto"].ToString(),
+                            cargocontacto = reader["cargocontacto"].ToString(),
+                            direccion = reader["direccion"].ToString()
+                        };
+                        listaProveedores.Add(proveedor);
+                    }
+                }
+
+                datagrid.ItemsSource = listaProveedores;
+            }
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string cadena = "Server=LAB1507-13\\SQLEXPRESS03; Database=NeptunoB; Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(cadena))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("USP_ListarCategorias", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                List<Categoria> listaCategorias = new List<Categoria>();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Categoria proveedor = new Categoria
+                        {
+                            idcategoria = reader["idcategoria"].ToString(),
+                            nombrecategoria = reader["nombrecategoria"].ToString(),
+                            descripcion = reader["descripcion"].ToString(),
+                            Activo = reader["Activo"].ToString(),
+                            CodCategoria = reader["CodCategoria"].ToString()
+                        };
+                        listaCategorias.Add(proveedor);
+                    }
+                }
+
+                datagrid2.ItemsSource = listaCategorias;
+            }
+        }
+    }
+    }
+}
